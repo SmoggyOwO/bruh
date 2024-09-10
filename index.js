ad96512f91dd57ae1e9fbb056beccf4dd6ed145c
SmoggyOwO <smoggyowo@gmail.com>
2024-09-10 13:32:44 +0530

    Bruh

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Route to respond with "Hello World"
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(3000);
